7508c717c880c347c6ee2224f352c459e762b826
palomalindinha01 <palomalindinha35@gmail.com>
2023-10-23 11:02:35 -0300

    first commit

---
+++ atividade11.py
@@ -0,0 +1,8 @@
+# Exercício Python 11: Desenvolva um programa que pergunte a distância de uma
+# viagem em Km. Calcule o preço da passagem, cobrando R$0,50 por Km para
+# viagens de até 200Km e R$0,45 parta viagens mais longas.
+dist1 = float(input("qual a distancia em km?"))
+if dist1<=200 
+    print(f"o valor da passagem é: {dist1*0.50}")
+else: 
+    print(f"o valor da passagem é: {dist1*0.45}")
